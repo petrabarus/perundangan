ab8fee2c830afc65d687515126da7f08f547b094
Petra Barus <petra.barus@gmail.com>
2014-03-08 05:08:50 +0700

    Removing unused footer and excessive <br> tag.

--- analyze_html.py
+++ analyze_html.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from os import rename, listdir
-from os.path import isfile, join
-from re import match
+from os.path import isfile, isdir, join
 from sys import argv
+import re
 from lxml import html, etree
-
 
 def clean1(filename, content):
     test = """
@@ -40,11 +40,25 @@ def clean2(filename, content):
         print filename
     return new_content
 
+def clean3(filename, content):
+    test = "<div class=\"d3\" align=\"right\"></div>"
+    if (test in content):
+        print filename
+        content = content.replace(test, '')
+    return content
+
+#Removing tailing br
+def clean4(filename, content):
+    new_content = content.replace('</div><br>', '</div>')
+    if (new_content != content):
+        print filename
+    return new_content
+
 def processfile(filename):
     fi = open(filename, "rb")
     content = fi.read()
     fi.close()
-    new_content = clean14(filename, content)
+    new_content = clean4(filename, content)
     fo = open(filename, "w")
     fo.write(new_content)
     fo.close()
@@ -54,6 +68,6 @@ if __name__ == '__main__':
         if (isfile(argv[1])):
             processfile(argv[1])
         elif (isdir(argv[1])):
-            for f in listdir(argv[1]):
+            for f in sorted(listdir(argv[1])):
                 filename = join(argv[1], f) 
                 processfile(filename)
