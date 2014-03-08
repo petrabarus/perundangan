5d017c2bfeb1b3596cbbcd95dfc7cf648a6f1011
Petra Barus <petra.barus@gmail.com>
2014-03-08 09:06:19 +0700

    Extracting PASAL

--- analyze_html.py
+++ analyze_html.py
@@ -122,12 +122,18 @@ def clean8(filename, content):
         print filename
     return new_content
 
+#Parse string PASAL XX in <center></center> and replace it with h4
+clean9regex1 = re.compile('Pasal (\d+)(\s+)</center>')
+def clean9(filename, content):
+    print filename
+    content = clean9regex1.sub('</center><h4>Pasal \g<1></h4>', content)
+    return content
 
 def processfile(filename):
     fi = open(filename, "rb")
     content = fi.read()
     fi.close()
-    new_content = clean8(filename, content)
+    new_content = clean9(filename, content)
     fo = open(filename, "w")
     fo.write(new_content)
     fo.close()
